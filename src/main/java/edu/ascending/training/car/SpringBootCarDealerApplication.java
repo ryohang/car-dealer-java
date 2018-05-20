77ecdabc2ebd6781ed09e947425fcb168df82f56
ryo <ryo.hang@gmail.com>
2018-05-20 00:04:44 -0400

    init db

---
+++ src/main/java/edu/ascending/training/car/SpringBootCarDealerApplication.java
@@ -0,0 +1,12 @@
+package edu.ascending.training.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCarDealerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCarDealerApplication.class, args);
+	}
+}
